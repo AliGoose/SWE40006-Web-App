de9eabe8a6eb61cce86c292b4f3a90e15de91f52
Lachlan <aligoose.martin@gmail.com>
2023-10-16 21:12:15 +1100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Page Title</title>
+</head>
+<body>
+    <?php include 'header.php'; ?>
 
-echo "Hello World!";
+    <?php include 'menu.php'; ?>
+
+    <!-- Your page content goes here -->
+
+    <?php include 'footer.php'; ?>
+
+</body>
+</html>
