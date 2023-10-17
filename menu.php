a02ec10c3cb53425d19f5506110037f47afd6be1
Lachlan <aligoose.martin@gmail.com>
2023-10-17 15:53:20 +1100

    remove calcualtor

--- menu.php
+++ menu.php
@@ -2,6 +2,5 @@
     <ul>
         <li><a href="index.php">Home</a></li>
         <li><a href="calendar.php">Calender</a></li>
-        <li><a href="calculator.php">Calculator</a></li>
     </ul>
 </nav>
